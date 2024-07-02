7297fdbca9d95f0f2ad2d0660f08b9d07b7a1222
DevNickjr <nicholasduadei14@gmail.com>
2024-07-02 20:05:40 +0100

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,18 +11,19 @@ export class AppService {
   ): Promise<IResponse> {
     const ipAddress = ip;
     const requestIP = request?.ip;
-    const socketIP = request?.socket?.remoteAddress;
+    const socketIP = request?.socket?.remoteAddress?.split(':');
     console.log({
       ipAddress,
       requestIP,
       socketIP,
     })
+    const ipVal = socketIP[socketIP.length - 1];
     try {
-      const res = await fetch(`https://ipapi.co/${socketIP}/json/`);
+      const res = await fetch(`https://ipapi.co/${ipVal}/json/`);
       const value = await res.json();
       console.log({ value });
       return {
-        client_ip: socketIP, // The IP address of the requester
+        client_ip: ipVal, // The IP address of the requester
         location: value?.city, // The city of the requester
         greeting: `Hello, ${name}!, the temperature is 11 degrees Celcius in ${value?.city}`,
       };
