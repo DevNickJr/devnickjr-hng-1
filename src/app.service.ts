5ac75ddafec6b564748516dfe5df3b0ec26c2f93
DevNickjr <nicholasduadei14@gmail.com>
2024-07-03 20:21:28 +0100

    push

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,27 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Request } from 'express';
 import { IResponse } from './interface';
+import requestIp from 'request-ip';
 
 @Injectable()
 export class AppService {
+  async getClientIp(request: Request) {
+    let ipAddress = request?.ip || request?.socket?.remoteAddress;
+
+    if (ipAddress && ipAddress.startsWith('::ffff:')) {
+      // Extract the IPv4 part from the IPv4-mapped IPv6 address
+      ipAddress = ipAddress.split(':').pop();
+    }
+
+    return ipAddress;
+  }
+
   async getHello(
     request: Request,
     ip: string,
     name: string,
   ): Promise<IResponse> {
+    const clientIp = requestIp.getClientIp(request)
     const ipAddress = ip;
     const requestIP = request?.ip;
     const socketIP = request?.socket?.remoteAddress?.split(':');
@@ -16,16 +29,22 @@ export class AppService {
       ipAddress,
       requestIP,
       socketIP,
+      clientIp,
     })
     const ipVal = socketIP[socketIP.length - 1];
     try {
-      const res = await fetch(`https://ipapi.co/${ipVal}/json/`);
+      const res = await fetch(`https://ipapi.co/${clientIp}/json/`);
       const value = await res.json();
       console.log({ value });
       return {
         client_ip: ipVal, // The IP address of the requester
         location: value?.city, // The city of the requester
         greeting: `Hello, ${name}!, the temperature is 11 degrees Celcius in ${value?.city}`,
+        ipAddress,
+        requestIP,
+        socketIP,
+        clientIp,
+        value,
       };
     } catch (error) {
       console.log({ error });
--- src/interface.ts
+++ src/interface.ts
@@ -2,4 +2,9 @@ export interface IResponse {
   client_ip: string;
   location: string;
   greeting: string;
+  ipAddress?: string;
+  requestIP?: string;
+  socketIP?: string | string[];
+  clientIp?: string;
+  value: any;
 }
