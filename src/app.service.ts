256b7dab1aba8b5e6a4d90c36e568ae4f691a3b9
DevNickjr <nicholasduadei14@gmail.com>
2024-07-04 12:06:56 +0100

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Request } from 'express';
 import { IResponse } from './interface';
-import requestIp from 'request-ip';
 
 @Injectable()
 export class AppService {
@@ -21,30 +20,29 @@ export class AppService {
     ip: string,
     name: string,
   ): Promise<IResponse> {
-    const clientIp = requestIp?.getClientIp(request);
     const ipAddress = ip;
     const heaerdIp = request.headers;
     const requestIP = request?.ip;
-    const socketIP = request?.socket?.remoteAddress?.split(':');
-    const ipVal = socketIP[socketIP.length - 1];
+    let socketIP = request?.socket?.remoteAddress;
+    if (socketIP.includes('::f')) {
+      const val = socketIP.split(':');
+      socketIP = val[val.length - 1];
+    }
     console.log({
       ipAddress,
       requestIP,
       socketIP,
-      clientIp,
       heaerdIp,
-      ipVal,
     });
     try {
-      const ipVal = socketIP[socketIP.length - 1];
       const res = await fetch(`
-        http://api.weatherapi.com/v1/ip.json?key=ca09e70b584049009be103752240407&q=${ipAddress || requestIP || socketIP}
+        http://api.weatherapi.com/v1/ip.json?key=ca09e70b584049009be103752240407&q=${socketIP || requestIP || socketIP}
       `);
       // const res = await fetch(`https://ipapi.co/${clientIp || heaerdIp || ipVal || requestIP}/json/`);
       const value = await res.json();
       console.log({ value });
       return {
-        client_ip: ipVal, // The IP address of the requester
+        client_ip: socketIP, // The IP address of the requester
         location: value?.city, // The city of the requester
         greeting: `Hello, ${name}!, the temperature is 11 degrees Celcius in ${value?.city}`,
         // ipAddress,
